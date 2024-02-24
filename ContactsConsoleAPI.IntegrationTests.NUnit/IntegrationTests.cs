7748af225ce4a739271a6abcd993ef7c44565ee3
Bozh <bojidarprofirov@abv.bg>
2024-02-24 18:28:09 +0200

    Add project files.

---
+++ ContactsConsoleAPI.IntegrationTests.NUnit/IntegrationTests.cs
@@ -0,0 +1,313 @@
+﻿using ContactsConsoleAPI.Business;
+using ContactsConsoleAPI.Business.Contracts;
+using ContactsConsoleAPI.Data.Models;
+using ContactsConsoleAPI.DataAccess;
+using ContactsConsoleAPI.DataAccess.Contrackts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactsConsoleAPI.IntegrationTests.NUnit
+{
+    public class IntegrationTests
+    {
+        private TestContactDbContext dbContext;
+        private IContactManager contactManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.dbContext = new TestContactDbContext();
+            this.contactManager = new ContactManager(new ContactRepository(this.dbContext));
+        }
+
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.dbContext.Database.EnsureDeleted();
+            this.dbContext.Dispose();
+        }
+
+
+        //positive test
+        [Test]
+        public async Task AddContactAsync_ShouldAddNewContact()
+        {
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+            await contactManager.AddAsync(newContact);
+
+            var dbContact = await dbContext.Contacts.FirstOrDefaultAsync(c => c.Contact_ULID == newContact.Contact_ULID);
+
+            Assert.NotNull(dbContact);
+            Assert.AreEqual(newContact.FirstName, dbContact.FirstName);
+            Assert.AreEqual(newContact.LastName, dbContact.LastName);
+            Assert.AreEqual(newContact.Phone, dbContact.Phone);
+            Assert.AreEqual(newContact.Email, dbContact.Email);
+            Assert.AreEqual(newContact.Address, dbContact.Address);
+            Assert.AreEqual(newContact.Contact_ULID, dbContact.Contact_ULID);
+        }
+
+        //Negative test
+        [Test]
+        public async Task AddContactAsync_TryToAddContactWithInvalidCredentials_ShouldThrowException()
+        {
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "invalid_Mail", //invalid email
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+            var ex = Assert.ThrowsAsync<ValidationException>(async () => await contactManager.AddAsync(newContact));
+            var actual = await dbContext.Contacts.FirstOrDefaultAsync(c => c.Contact_ULID == newContact.Contact_ULID);
+
+            Assert.IsNull(actual);
+            Assert.That(ex?.Message, Is.EqualTo("Invalid contact!"));
+
+        }
+
+        [Test]
+        public async Task DeleteContactAsync_WithValidULID_ShouldRemoveContactFromDb()
+        {
+            // Arrange
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+            await contactManager.AddAsync(newContact);
+            // Act
+            await contactManager.DeleteAsync(newContact.Contact_ULID);
+            // Assert
+            var contactInDb = await dbContext.Contacts.FirstOrDefaultAsync(x => x.Contact_ULID == newContact.Contact_ULID);
+            Assert.IsNull(contactInDb);
+        }
+
+        [TestCase("")]
+        [TestCase("    ")]
+        [TestCase(null)]
+        public async Task DeleteContactAsync_TryToDeleteWithNullOrWhiteSpaceULID_ShouldThrowException(string invalidUlid)
+        {
+
+            Assert.Throws<ArgumentException>(() => contactManager.DeleteAsync(invalidUlid));
+        }
+
+        [Test]
+        public async Task GetAllAsync_WhenContactsExist_ShouldReturnAllContacts()
+        {
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+            await contactManager.AddAsync(newContact);
+
+            var result = await contactManager.GetAllAsync();
+
+            Assert.That(result.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task GetAllAsync_WhenNoContactsExist_ShouldThrowKeyNotFoundException()
+        {
+            var expection = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.GetAllAsync());
+
+            Assert.That(expection.Message, Is.EqualTo("No contact found."));
+        }
+
+        [Test]
+        public async Task SearchByFirstNameAsync_WithExistingFirstName_ShouldReturnMatchingContacts()
+        {
+            // Arrange
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+
+
+            var secondContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+            await contactManager.AddAsync(newContact);
+            await contactManager.AddAsync(secondContact);
+            // Act
+            var result = await contactManager.SearchByFirstNameAsync(secondContact.FirstName);
+            // Assert
+            Assert.That(result.Count, Is.EqualTo(2));
+            var itemInDb = result.First();
+            Assert.That(itemInDb.LastName, Is.EqualTo(secondContact.LastName));
+        }
+
+        [Test]
+        public async Task SearchByFirstNameAsync_WithNonExistingFirstName_ShouldThrowKeyNotFoundException()
+        {
+            var exeption = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.SearchByFirstNameAsync("NO_SUCH_KEY"));
+            Assert.That(exeption.Message, Is.EqualTo("No contact found with the given first name."));
+        }
+
+        [Test]
+        public async Task SearchByLastNameAsync_WithExistingLastName_ShouldReturnMatchingContacts()
+        {
+
+
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+
+            await contactManager.AddAsync(newContact);
+
+            var result = await contactManager.SearchByLastNameAsync(newContact.LastName);
+            Assert.That(result.Count(), Is.EqualTo(1));
+        }
+
+        [Test]
+        public async Task SearchByLastNameAsync_WithNonExistingLastName_ShouldThrowKeyNotFoundException()
+        {
+            var exception = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.SearchByLastNameAsync("NON_EXISTING_NAME"));
+            Assert.That(exception.Message, Is.EqualTo("No contact found with the given last name."));
+        }
+
+        [Test]
+        public async Task GetSpecificAsync_WithValidULID_ShouldReturnContact()
+        {
+            var newContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+            var secondContact = new Contact()
+            {
+                FirstName = "TestFirstName",
+                LastName = "TestLastName",
+                Address = "Anything for testing address",
+                Contact_ULID = "1ABC23456HH", //must be minimum 10 symbols - numbers or Upper case letters
+                Email = "test@gmail.com",
+                Gender = "Male",
+                Phone = "0889933779"
+            };
+            await contactManager.AddAsync(newContact);
+            await contactManager.AddAsync(secondContact);
+
+            var result = await contactManager.GetSpecificAsync(newContact.Contact_ULID);
+            Assert.NotNull(result);
+            Assert.That(result.Equals(newContact), Is.True);
+        }
+
+        [Test]
+        public async Task GetSpecificAsync_WithInvalidULID_ShouldThrowKeyNotFoundException()
+        {
+            const string invalidUlid = "NON_VALID_ID";
+            var exception = Assert.ThrowsAsync<KeyNotFoundException>(() => contactManager.GetSpecificAsync(invalidUlid));
+
+            Assert.That(exception.Message, Is.EqualTo($"No contact found with ULID: {invalidUlid}"));
+        }
+
+        [Test]
+        public async Task UpdateAsync_WithValidContact_ShouldUpdateContact()
+        {
+            var newContacts = new List<Contact>()
+           {
+               new Contact()
+               {
+                 FirstName = "Gunq",
+                 LastName = "Qsha",
+                 Address = "Ima lee",
+                 Contact_ULID = "1234567654ABBI",
+                 Email = "bagamiash@gmail.com",
+                 Gender = "Male",
+                 Phone = "0884745542"
+
+               },
+
+               new Contact()
+               {
+                   FirstName = "Gunqrr",
+                 LastName = "Qsharr",
+                 Address = "Imarrr lee",
+                 Contact_ULID = "1234567654ABBI55",
+                 Email = "bagmiash@gmail.com",
+                 Gender = "Female",
+                 Phone = "0984745542"
+               }
+           };
+            foreach (var contact in newContacts)
+            { 
+              await contactManager.AddAsync(contact);
+            }
+            var modifiedContact = newContacts[0];
+            modifiedContact.FirstName = "UPDATED!";
+
+            await contactManager.UpdateAsync(modifiedContact);
+
+            var itemInDb = await dbContext.Contacts.FirstOrDefaultAsync(x => x.Contact_ULID == modifiedContact.Contact_ULID);
+
+            Assert.NotNull(itemInDb);
+            Assert.That(itemInDb.FirstName, Is.EqualTo(modifiedContact.FirstName));
+        }
+       
+
+        [Test]
+        public async Task UpdateAsync_WithInvalidContact_ShouldThrowValidationException()
+        {
+            var exception = Assert.ThrowsAsync<ValidationException>(() => contactManager.UpdateAsync(new Contact()));
+            Assert.That(exception.Message, Is.EqualTo("Invalid contact!"));
+        }
+    }
+}
